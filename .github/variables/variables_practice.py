81b36a932d898ebd65f7e45627c83f998a758238
TylerCSmythe <TylerSmythe688@gmail.com>
2021-10-29 09:21:48 -0400

    Update variables_practice.py
    Created an integer and floating point variable.

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,1 +1,6 @@
-# Variables Practice, Tyler Smith, 10/29/21 09:05am, version 0.0
+# Variables Practice, Tyler Smith, 10/29/21 09:20am, version 0.1
+
+# Three Main Types of Variables for this Class 
+
+health = 100 # INTEGERS, any +/- whole number AND 0. 
+account_balance =12345.52 # FLOATING POINT, any +/- with decimal.
