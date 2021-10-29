9949fa8154b83c352f6c0be1abd9ce48511f80c2
TylerCSmythe <TylerSmythe688@gmail.com>
2021-10-29 09:33:00 -0400

    Update variables_practice.py
    Added STRING letters.

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,6 +1,11 @@
-# Variables Practice, Tyler Smith, 10/29/21 09:20am, version 0.1
+# Variables Practice, Tyler Smith, 10/29/21 09:32am, version 0.1
 
 # Three Main Types of Variables for this Class 
 
 health = 100 # INTEGERS, any +/- whole number AND 0. 
 account_balance =12345.52 # FLOATING POINT, any +/- with decimal.
+user_name = "Thomas Edison" # STRING, letters/numbers printed on screen.
+
+print(health)
+print(account_balance)
+print(user_name)
